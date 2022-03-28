73dad4939e8a947487433d53c691a13c19b641b7
Jonszr <s13712651652@gmail.com>
2022-03-27 22:35:50 -0600

    src-first commit

---
+++ src/main/java/ca/sait/lab7/models/generated/User.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sait.lab7.models.generated;
+
+/**
+ *
+ * @author JonSZR
+ */
+public class User {
+    
+}
